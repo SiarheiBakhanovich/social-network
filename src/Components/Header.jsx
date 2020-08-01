c9d96180a7fa608c0605c62ec3b7fd5be268738b
Siarhei <bahanovik@gmail.com>
2020-08-01 20:57:28 +0300

    css-modules

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='http://vector30.com/wp-content/uploads/2018/01/%D0%BB%D0%BE%D0%B3%D0%BE-1024x576.jpg'></img>
         </header>
     )
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+import  s from'./Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <img
                 src='https://www.wallpaperup.com/uploads/wallpapers/2013/07/01/112481/62c30d7e73033ad264c790b63b233bba-1000.jpg'></img>
 
             <div>
-                ava + descroption
+                ava + description
             </div>
             <div>
                 My posts
                 <div>
                     New post
                 </div>
-                <div>
-                    post 1
-                </div>
-                <div>
-                    post 2
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post 1
+                    </div>
+                    <div className={s.item}>
+                        post 2
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+            )
+            }
 
-export default Profile;
+            export default Profile;
