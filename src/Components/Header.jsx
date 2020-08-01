5cecfca16cfb82794b4f091f753b613d30082f1e
Siarhei <bahanovik@gmail.com>
2020-08-01 20:11:20 +0300

    components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='http://vector30.com/wp-content/uploads/2018/01/%D0%BB%D0%BE%D0%B3%D0%BE-1024x576.jpg'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img
+                src='https://www.wallpaperup.com/uploads/wallpapers/2013/07/01/112481/62c30d7e73033ad264c790b63b233bba-1000.jpg'></img>
+
+            <div>
+                ava + descroption
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
