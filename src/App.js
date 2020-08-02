4212a21820891d04bc2f14b7950f648b9f386eda
Siarhei <bahanovik@gmail.com>
2020-08-02 16:33:48 +0300

    NavLink

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
-
+import {BrowserRouter,  } from "react-router-dom";
+import {Route} from "react-router-dom";
 const App = (props) => {
     return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
 
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+                </div>
+
             </div>
-
-            {/*<Profile/>*/}
-
-        </div>
+        </BrowserRouter>
     );
 }
 
