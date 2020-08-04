63dbdfa1330bded876ee3f7b11a50cb96ed489a8
Siarhei <bahanovik@gmail.com>
2020-08-04 15:16:37 +0300

    24

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -20,28 +20,48 @@ const Message = (props) => {
             )
             }
 
+let dialogsData = [
+    {id: 1, name: 'Serhio'},
+    {id: 2, name: 'Justin'},
+    {id: 3, name: 'Toni'},
+    {id: 4, name: 'Peter'},
+    {id: 5, name: 'Katy'},
+    {id: 6, name: 'Kim'}
+]
+
+let messagesData = [
+    {id: 1, message: 'helloo'},
+    {id: 2, message: 'i am fine'},
+    {id: 3, message: 'commone'},
+    {id: 4, message: 'Notorius BIG'},
+    {id: 5, message: 'how?'},
+    {id: 6, message: 'brr'}
+]
+
             const Dialogs = (props) => {
             return (
             <div className={s.dialogs}>
             <div className={s.dialogsItems}>
 
-            <DialogItem name="Serhio" id="1"/>
-            <DialogItem name="Justin" id="2"/>
-            <DialogItem name="Toni" id="3"/>
-            <DialogItem name="Peter" id="4"/>
-            <DialogItem name="Katy" id="5"/>
-            <DialogItem name="Kim" id="6"/>
+            <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+            <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+            <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+            <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+            <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+            <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
 
             </div>
             <div className={s.messages}>
-            <Message mesaage="Hi" />
-                <Message message="How is your live?" />
-                <Message message="Fantastic" />
-                <Message message="Hi" />
+            <Message mesaage={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
+                <Message message={messagesData[3].message} />
 
             </div>
             </div>
             )
         }
+
+
 
             export default Dialogs;
